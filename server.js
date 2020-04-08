8a23c63be6fc847a34f473d1989c51bf0c5879f6
Miguel Tineo <mtineo@zendesk.com>
2020-04-08 14:47:10 +0100

    Changes

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 var path = require('path');
 
+app.set('view engine', 'pug');
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  // res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.render('index', {lat: 0, lng: 0})
   // res.send('Hello miguel is here: ');
 });
 
